f334c0f4e6f3cd941a422e9974383318c74ddcd6
salam <chaudharysm00@gmail.com>
2024-10-14 14:37:12 +0545

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,8 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-800 text-center text-gray-200 py-8 px-4">
+      <p className="text-sm">&copy; 2024 CdyEats. All rights reserved.</p>
+    </footer>
+  );
 };
 export default Footer;
