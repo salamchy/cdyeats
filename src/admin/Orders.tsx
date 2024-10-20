fa5a72f8f052247af85ea7b7591b0e0e7c4cd015
salam <chaudharysm00@gmail.com>
2024-10-20 20:27:23 +0545

    orders

---
+++ src/admin/Orders.tsx
@@ -0,0 +1,10 @@
+const Orders = () => {
+  return (
+    <div className="max-w-6xl mx-auto py-10 px-6">
+      <h1 className="text-3xl font-extrabold text-gray-900 dark:text-white mb-10">
+        Orders Overview
+      </h1>
+    </div>
+  );
+};
+export default Orders;
