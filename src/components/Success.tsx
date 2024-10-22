37f7df3b5fa7df17d664e8ab96fc8b97327af03c
salam <chaudharysm00@gmail.com>
2024-10-22 19:48:22 +0545

    success

---
+++ src/components/Success.tsx
@@ -0,0 +1,23 @@
+const Success = () => {
+  const orders = [];
+
+  if (orders.length === 0) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="font-bold text-2xl text-gray-700 dark:text-gray-300">
+          Order not found!
+        </h1>
+      </div>
+    );
+  }
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900 px-4">
+      <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 max-w-lg w-full">
+        <div className="text-center mb-6">
+          <h1>Order Status</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Success;
